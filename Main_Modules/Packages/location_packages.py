0db69da5741c22093885a328917000d802c140c7
salaaaheddine <theonegreat0@gmail.com>
2026-01-13 13:49:11 +0100

    Packages Migration Updated

--- Main_Modules/Packages/location_packages.py
+++ Main_Modules/Packages/location_packages.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, text, Engine, NVARCHAR, DECIMAL
 from urllib.parse import quote_plus
 import pandas as pd
 from utils.tools import get_logger
+from utils.custom_err import IncrementalDependencyError
 
 log = get_logger('LocationPackages')
 warnings.filterwarnings('ignore')
@@ -31,17 +32,13 @@ def source_db_conn(): return get_engine('AZURE_SERVER','AZURE_DATABASE','AZURE_U
 def target_db_conn(): return get_engine('STAGE_SERVER','STAGE_DATABASE','STAGE_USERNAME','STAGE_PASSWORD')
 
 # -------------------- Extract --------------------
-def extract(engine: Engine) -> pd.DataFrame:
-    """Extract data based on CDC."""
-    with engine.begin() as conn:
-        max_id = conn.execute(
-            text("SELECT ISNULL(MaxIndex,0) FROM app.EtlCDC WHERE TableName=:table_name"),
-            {"table_name": 'app.LocationPackages'}
-        ).scalar()
-    
-    max_id = max_id if not max_id is None else 0
-    log.info(f'Current CDC for app.LocationPackages: {max_id}')
-    query = f"SELECT TOP 500 PackageID, CategoryID, Price, CreatedAt, UpdatedAt, StatusID FROM app.Packages WHERE PackageID > {max_id} ORDER BY PackageID "
+def extract(user_id:int, engine: Engine) -> pd.DataFrame:
+    """Extract data based on UserID."""
+
+    account_id = pd.read_sql(f'SELECT AccountID FROM app.Accounts WHERE OldUserID={user_id}', engine)
+    account_id = int(account_id['AccountID']) # type: ignore
+
+    query = f"SELECT PackageID, AccountID, CategoryID, Price, CreatedAt, UpdatedAt, StatusID FROM app.Packages WHERE AccountID={account_id} ORDER BY PackageID"
     df = pd.read_sql_query(query, engine)
     log.info(f'Extracted {len(df)} rows from app.Packages')
     return df
@@ -50,25 +47,18 @@ def extract(engine: Engine) -> pd.DataFrame:
 def transform(df: pd.DataFrame, engine: Engine) -> pd.DataFrame:
     """Clean and transform Packages data."""
 
-    print(df)
-
-    # Sync AccountID and CategoryID
-    cat_acc_ids = pd.read_sql("SELECT AccountID, CategoryID FROM app.Categories", engine)
+    # Get LocationIDs
     loc_acc_ids = pd.read_sql(f"SELECT AccountID, LocationID FROM app.Locations", engine)
-
-
-    df = pd.merge(df, cat_acc_ids, on='CategoryID', how='left')
     df = pd.merge(df, loc_acc_ids, on='AccountID', how='left')
-
+    missing_loc = df['LocationID'].isna().sum()
+    if missing_loc:
+        log.warning(f'Missing LocationIDs: {missing_loc}')
+        raise IncrementalDependencyError('Update Categories Table.')
 
 
     df.drop(columns={'CategoryID', 'AccountID'}, inplace=True)
 
-
-    print(df)
-
-
-    log.info(f'Transformation complete, df\'s Length is {len(df)}')
+    log.info(f'Transformation complete. df rows: {len(df)}')
     return df
 
 # -------------------- Load --------------------
@@ -76,43 +66,30 @@ def load(df: pd.DataFrame, engine: Engine):
 
     dtype_mapping = {col:NVARCHAR(None) for col in df.select_dtypes(include='object').columns}
     
-    max_id = df['PackageID'].max()
-
     try:
         with engine.begin() as conn:  # Transaction-safe
 
             df.to_sql('LocationPackages', con=conn, schema='app', if_exists='append', index=False, dtype=dtype_mapping) # type: ignore
             log.info(f'app.LocationPackages loaded successfully')
 
-            conn.execute(
-                text("""
-                    MERGE app.[EtlCDC] AS target
-                    USING (SELECT :table_name AS [TableName], :max_index AS [MaxIndex]) AS source
-                    ON target.[TableName] = source.[TableName]
-                    WHEN MATCHED THEN UPDATE SET target.[MaxIndex] = source.[MaxIndex]
-                    WHEN NOT MATCHED THEN INSERT ([TableName],[MaxIndex]) VALUES (source.[TableName],source.[MaxIndex]);
-                """),
-                {"table_name": f'app.LocationPackages', "max_index": int(max_id)}
-            )
-            log.info(f'app.LocationPackages loaded successfully, CDC updated to {max_id}')
     except Exception as e:
         log.error(f'Failed to load app.LocationPackages: {e}')
         raise
 
 # -------------------- Main --------------------
-def main():
+def main(user_id:int, if_load:bool=True):
     target = target_db_conn()
 
-    while True:
-        df = extract(target)
-        if df.empty:
-            log.info('No new data to load.')
-            break
-        df = transform(df, target)
-        # print(df.head(20))
-        # return
-        load(df, target)
-        # return
+    df = extract(user_id, target)
+    if df.empty:
+        log.info('No data to load.')
+        return
+    
+    df = transform(df, target)
+    print(df)
 
-if __name__ == '__main__':
-    main()
+    if if_load:
+        load(df, target)
+
+# if __name__ == '__main__':
+#     main()
