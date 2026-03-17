601367a83a7421a5b4266ac457bb4f4cd3291e96
salaaaheddine <theonegreat0@gmail.com>
2026-03-17 12:29:08 +0000

    Reconciliations migration added

--- Invertory/Reconciliations/reconciliations.py
+++ Invertory/Reconciliations/reconciliations.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from sqlalchemy import create_engine, text, Engine, NVARCHAR, DECIMAL
 from urllib.parse import quote_plus
 import pandas as pd
-from utils.tools import get_logger
+from utils.tools import get_last_ingested, get_logger, update_last_ingested
 from utils.fks_mapper import get_suppliers, get_warehouses
 from utils.custom_err import IncrementalDependencyError
 
@@ -33,18 +33,13 @@ def source_db_conn(): return get_engine('AZURE_SERVER','AZURE_DATABASE','AZURE_U
 def target_db_conn(): return get_engine('STAGE_SERVER','STAGE_DATABASE','STAGE_USERNAME','STAGE_PASSWORD')
 
 # -------------------- Extract --------------------
-def extract(source_db: Engine, target_db: Engine) -> pd.DataFrame:
-    """Extract data based on CDC."""
-    with target_db.begin() as conn:
-        max_id = conn.execute(
-            text("SELECT ISNULL(MaxIndex,0) FROM app.ETLcdc WHERE TableName=:table_name"),
-            {"table_name": 'dbo.inv_Reconciliation'}
-        ).scalar()
-    max_id = max_id if not max_id is None else 0
-    log.info(f'Current CDC for dbo.inv_Reconciliation: {max_id}')
+def extract(user_id: int, engine: Engine) -> pd.DataFrame:
+    """Extract data based on UserID."""
 
-    query = f"SELECT top 100 * FROM dbo.inv_Reconciliation WHERE ReconciliationID > {max_id} ORDER BY ReconciliationID"
-    df = pd.read_sql_query(query, source_db)
+    last_id = get_last_ingested(user_id, 'dbo.inv_Reconciliation')
+
+    query = f"SELECT * FROM dbo.inv_Reconciliation WHERE UserID={user_id} AND ReconciliationID > {last_id} ORDER BY ReconciliationID"
+    df = pd.read_sql_query(query, engine)
     log.info(f'Extracted {len(df)} rows from dbo.inv_Reconciliation')
     return df
 
@@ -75,10 +70,6 @@ def transform(df: pd.DataFrame, engine: Engine) -> pd.DataFrame:
     if missing_whs:
         log.warning(f'Missing WarehouseIDs: {missing_whs}')
         raise IncrementalDependencyError('Update Warehouses Table.')
-    
-    # // TO DO: AuditedByUserID
-
-
 
 
     df.drop(columns=[
@@ -90,12 +81,10 @@ def transform(df: pd.DataFrame, engine: Engine) -> pd.DataFrame:
     return df
 
 # -------------------- Load --------------------
-def load(df: pd.DataFrame, engine: Engine):
+def load(df: pd.DataFrame, user_id: int, engine: Engine):
 
     dtype_mapping = {col:NVARCHAR(None) for col in df.select_dtypes(include='object').columns}    
     
-    max_id = df['OldReconciliationID'].max()
-
     try:
         with engine.begin() as conn:  # Transaction-safe
 
@@ -113,39 +102,28 @@ def load(df: pd.DataFrame, engine: Engine):
             log.info("Verified/Added OldReconciliationID column.")
 
             df.to_sql('Reconciliations', con=conn, schema='app', if_exists='append', index=False, dtype=dtype_mapping) # type: ignore
+            update_last_ingested(user_id, 'dbo.inv_Reconciliation', int(df['OldReconciliationID'].max()))
             log.info(f'dbo.inv_Reconciliation loaded successfully')
 
-            conn.execute(
-                text("""
-                    MERGE app.[ETLcdc] AS target
-                    USING (SELECT :table_name AS [TableName], :max_index AS [MaxIndex]) AS source
-                    ON target.[TableName] = source.[TableName]
-                    WHEN MATCHED THEN UPDATE SET target.[MaxIndex] = source.[MaxIndex]
-                    WHEN NOT MATCHED THEN INSERT ([TableName],[MaxIndex]) VALUES (source.[TableName],source.[MaxIndex]);
-                """),
-                {"table_name": f'dbo.inv_Reconciliation', "max_index": int(max_id)}
-            )
-            log.info(f'dbo.inv_Reconciliation loaded successfully, CDC updated to {max_id}')
     except Exception as e:
         log.error(f'Failed to load dbo.inv_Reconciliation: {e}')
         raise
 
 # -------------------- Main --------------------
-def main():
+def main(user_id:int, if_load:bool=True):
     source = source_db_conn()
     target = target_db_conn()
-    # return
-    while True:
-        df = extract(source, target)
-        if df.empty:
-            log.info('No new data to load.')
-            return
-        
-        # return
-        df = transform(df, target)
-        # print(df)
-        # return
-        load(df, target)
 
-if __name__ == '__main__':
-    main()
+    df = extract(user_id, source)
+    if df.empty:
+        log.info('No data to load.')
+        return 
+
+    df = transform(df, target)
+    print(df)
+
+    if if_load:
+        load(df, user_id, target)
+
+# if __name__ == '__main__':
+#     main()
