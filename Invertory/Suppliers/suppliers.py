93ec3628e9a81d3c3ec827bc20f92592c7b0e49a
salaaaheddine <theonegreat0@gmail.com>
2026-03-17 12:33:28 +0000

    Batch inserting added

--- Invertory/Suppliers/suppliers.py
+++ Invertory/Suppliers/suppliers.py
@@ -5,7 +5,9 @@ from datetime import datetime
 from sqlalchemy import create_engine, text, Engine, NVARCHAR, DECIMAL
 from urllib.parse import quote_plus
 import pandas as pd
-from utils.tools import get_logger
+from utils.custom_err import IncrementalDependencyError
+from utils.fks_mapper import get_accounts
+from utils.tools import get_last_ingested, get_logger, clean_contact, update_last_ingested
 
 
 warnings.filterwarnings('ignore')
@@ -33,30 +35,25 @@ def source_db_conn(): return get_engine('AZURE_SERVER','AZURE_DATABASE','AZURE_U
 def target_db_conn(): return get_engine('STAGE_SERVER','STAGE_DATABASE','STAGE_USERNAME','STAGE_PASSWORD')
 
 # -------------------- Extract --------------------
-def extract(source_db: Engine, target_db: Engine) -> pd.DataFrame:
-    """Extract data based on CDC."""
-    with target_db.begin() as conn:
-        max_id = conn.execute(
-            text("SELECT ISNULL(MaxIndex,0) FROM app.EtlCDC WHERE TableName=:table_name"),
-            {"table_name": 'dbo.Supplier'}
-        ).scalar()
-    
-    max_id = max_id if not max_id is None else 0
-    log.info(f'Current CDC for dbo.Supplier: {max_id}')
+def extract(user_id: int, engine: Engine) -> pd.DataFrame:
+    """Extract data based on UserID."""
 
-    query = f"SELECT TOP 1000 * FROM dbo.Supplier WHERE SupplierID > {max_id}"
-    df = pd.read_sql_query(query, source_db)
+    last_id = get_last_ingested(user_id, 'dbo.Supplier')
+
+    query = f"SELECT * FROM dbo.Supplier WHERE UserID={user_id} AND SupplierID > {last_id} ORDER BY SupplierID"
+    df = pd.read_sql_query(query, engine)
     log.info(f'Extracted {len(df)} rows from dbo.Supplier')
     return df
 
 # -------------------- Transform --------------------
-def transform(df: pd.DataFrame, source_db: Engine, target_db: Engine) -> pd.DataFrame:
+def transform(df: pd.DataFrame, engine: Engine) -> pd.DataFrame:
     """Clean and transform Suppliers data."""
     # Keep only necessary columns and rename
-    df = df[['SupplierID', 'Name' , 'Email', 'Phone', 'ContactPerson', 'Address', 'StatusID', 'CreatedOn', 'LastUpdatedDate']]
+    df = df[['SupplierID', 'UserID', 'Name' , 'Email', 'Phone', 'ContactPerson', 'Address', 'StatusID', 'CreatedOn', 'LastUpdatedDate']]
 
     df.rename(columns={
         "SupplierID":'OldSupplierID',
+        'UserID':'OldUserID',
         "Email":"ContactEmail",
         "Phone":"ContactPhone",
         "Address":"NationalAddress",
@@ -71,6 +68,7 @@ def transform(df: pd.DataFrame, source_db: Engine, target_db: Engine) -> pd.Data
             else: 
                 df[col] = df[col].apply(lambda x: x.strip() if isinstance(x,str) else x)
 
+    df['ContactPhone'] = df['ContactPhone'].map(clean_contact)
 
     # Filling Null Values in StatusID
     df['StatusID'] = df['StatusID'].fillna(1)
@@ -79,16 +77,22 @@ def transform(df: pd.DataFrame, source_db: Engine, target_db: Engine) -> pd.Data
     df["IsVATRegistered"] = 0
 
 
+    df = pd.merge(df, get_accounts(engine), on='OldUserID', how='left')
+    missing_acc = df['AccountID'].isna().sum()
+    if missing_acc:
+        log.warning(f'Missing AccountIDs: {missing_acc}')
+        raise IncrementalDependencyError("Update Accounts Table.")
+    
+    df.drop(columns='OldUserID', inplace=True)
+
     log.info(f'Transformation complete, df\'s Length is {len(df)}')
     return df
 
 # -------------------- Load --------------------
-def load(df: pd.DataFrame, engine: Engine):
+def load(df: pd.DataFrame, user_id: int, engine: Engine):
 
     dtype_mapping = {col:NVARCHAR(None) for col in df.select_dtypes(include='object').columns}
     
-    max_id = df['OldSupplierID'].max()
-
     try:
         with engine.begin() as conn:  # Transaction-safe
 
@@ -106,37 +110,29 @@ def load(df: pd.DataFrame, engine: Engine):
             log.info("Verified/Added OldSupplierID column.")
 
             df.to_sql('Suppliers', con=conn, schema='app', if_exists='append', index=False, dtype=dtype_mapping) # type: ignore
+            update_last_ingested(user_id, 'dbo.Supplier', int(df['OldSupplierID'].max()))
             log.info(f'dbo.Supplier loaded successfully')
 
-            conn.execute(
-                text("""
-                    MERGE app.[EtlCDC] AS target
-                    USING (SELECT :table_name AS [TableName], :max_index AS [MaxIndex]) AS source
-                    ON target.[TableName] = source.[TableName]
-                    WHEN MATCHED THEN UPDATE SET target.[MaxIndex] = source.[MaxIndex]
-                    WHEN NOT MATCHED THEN INSERT ([TableName],[MaxIndex]) VALUES (source.[TableName],source.[MaxIndex]);
-                """),
-                {"table_name": f'dbo.Supplier', "max_index": int(max_id)}
-            )
-            log.info(f'dbo.Supplier loaded successfully, CDC updated to {max_id}')
+    
     except Exception as e:
         log.error(f'Failed to load dbo.Supplier: {e}')
         raise
 
 # -------------------- Main --------------------
-def main():
+def main(user_id:int, if_load:bool=True):
     source = source_db_conn()
     target = target_db_conn()
 
-    while True:
-        df = extract(source, target)
-        if df.empty:
-            log.info('No new data to load.')
-            break
-        df = transform(df, source, target)
-        # print(df.head(20))
-        # return
-        load(df, target)
+    df = extract(user_id, source)
+    if df.empty:
+        log.info('No data to load.')
+        return 
 
-if __name__ == '__main__':
-    main()
+    df = transform(df, target)
+    print(df)
+
+    if if_load:
+        load(df, user_id, target)
+
+# if __name__ == '__main__':
+#     main()
